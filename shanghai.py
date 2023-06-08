052f95d23c349e76301fed641922ff6a8e8aef6c
traceandmark <115900504+traceandmark@users.noreply.github.com>
2023-06-09 07:38:01 +0800

    Update shanghai.py

--- shanghai.py
+++ shanghai.py
@@ -6,11 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route('/home')
-def home(name):
-    session['name']= name
-    return render_template("home.html")
+@app.route('/flavors/<shangrice_flavor>')
+def flavors(shangrice_flavor):
+    flavor = shangrice_flavor
+    return render_template("products.html", name = flavor )
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+@app.route('/flavors/<shangrice_flavor>/<database_flavor>')
+def database(shangrice_flavor,database_flavor):
+    flavor = shangrice_flavor
+    data = database_flavor
+    return render_template("database.html", name = flavor, name2 = data )
+
+@app.route('/flavors/<shangrice_flavor>/<database_flavor>/<database_type>')
+def databasetype(shangrice_flavor,database_flavor,database_type):
+    flavor = shangrice_flavor
+    data = database_flavor
+    datatype = database_type
+    return render_template("databasetype.html", name = flavor, name2 = data, name3 = datatype )
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
